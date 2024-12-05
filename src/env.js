1cb07286ae358f8563681a3fc6e2bec5e060b1df
gowfin <emm4christ@yahoo.com>
2024-12-05 01:04:41 +0100

    more report added

--- src/env.js
+++ src/env.js
@@ -2,8 +2,8 @@
           export const getLocalhost = (companyName) => {
                if (!companyName) {return "https://gowfinbackend.onrender.com";}
                else if (companyName.toUpperCase() === 'BLESSED') {
-                      return 'http://localhost:3005';  
-                //   return 'https://gowfinblessedbackend.vercel.app';
+                    //   return 'http://localhost:3005';  
+                  return 'https://gowfinblessedbackend.vercel.app';
                
                    
                }
