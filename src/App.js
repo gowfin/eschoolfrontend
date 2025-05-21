26fdcde9363b5c91a9cafcf7e27a2e21bd513035
gowfin <68777205+gowfin@users.noreply.github.com>
2025-05-21 11:01:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Quiz from './quiz';
 import LandingPage from './LandingPage';
 
